ee41238d1e4c66b3f807616498e7916ccaab0479
Zakeshu <raekandery@gmail.com>
2015-10-19 20:38:48 +0300

    dz-18-19

---
+++ addressbook-native-tests/src/com/example/tests/TestContactDeletion.java
@@ -0,0 +1,11 @@
+package com.example.tests;
+
+import org.testng.annotations.Test;
+
+public class TestContactDeletion extends TestBase {
+
+	@Test
+	public void shouldDeleteContact() {
+		app.getContactHelper().deleteContact();
+	}
+}
